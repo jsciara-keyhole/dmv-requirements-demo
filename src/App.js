6e2d59e669fd58310c887fb7550dbf0a06f4f430
AD\joe.sciara <joe.sciara@acertusdelivers.com>
2022-07-19 11:22:18 -0500

    changes for v3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Version 2.
+          Hello from Version 22.
         </p>
         <a
           className="App-link"
